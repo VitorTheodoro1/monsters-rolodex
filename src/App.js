ff08dd2254792bc49efce620ec6bcab81db96a11
Vitor Theodoro <vitor-theodoro@hotmail.com>
2022-05-22 16:29:39 -0300

    add: monsters rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+//import { Component } from "react";
+import CardList from "./components/card-list/card-list-component";
+import SearchBox from "./components/search-box/search-file-component";
 
-function App() {
+const App = () => {
+  const [searchField, setSearchField] = useState(""); // [value, setValue]
+  const [monsters, setMonsters] = useState([]); // [value, setValue]
+  const [filteredMonsters, setFilterMonsters] = useState(monsters); // [value, setValue]
+  console.log("render");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users") //pegar dados
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      // como search field está trocando o objeto, ele chama render novamente e essa funcao consegue ser executada
+      //true ou falso pra cada um
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      ></SearchBox>{" "}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
-}
+};
+
+//fazendo com class component
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     //quando o componente for montado pela primeira vez
+//     fetch("https://jsonplaceholder.typicode.com/users") //pegar dados
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(
+//           () => {
+//             return { monsters: users };
+//           },
+//           () => {}
+//         )
+//       );
+//   }
+
+//   onSearchChange = (event) => {
+//     // criou essa funcao para otimizacao só cria uma vez e pronto
+//     //const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return { searchField: event.target.value.toLocaleLowerCase() }; // searchField: searchField
+//     });
+//   };
+
+//   render() {
+//     const { monsters, searchField } = this.state; // fez essas campos pra não ficar utilizando this toda hora
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       // como search field está trocando o objeto, ele chama render novamente e essa funcao consegue ser executada
+//       //true ou falso pra cada um
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox
+//           className="monsters-search-box"
+//           onChangeHandler={onSearchChange}
+//           placeholder="search monsters"
+//         ></SearchBox>
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
+
+/* IMPORTANTISSIMO
+
+//Problema do objeto antigo do gta dourado
+
+ <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return {
+                    name: { firstName: "Andrei", lastName: "Neaogoi" },
+                  };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
+
+
+  //será que consigo fazer isso em campos do modal?
+
+{this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
+
+
+// problema do warning unique key 
+//coloqueio id no h1
+
+ monsters: [
+        {
+          id: "83tbw8fihweb",
+          name: "Linda",
+        },
+ return <h1 key={monster.id}>{monster.name}</h1>;
+
+
+
+
+*/
---
+++ src/components/search-box/search-file-component.jsx
@@ -0,0 +1,13 @@
+//import { Component } from "react";
+import "./search-box-styles.css";
+
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={` search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  ></input>
+);
+
+export default SearchBox;
